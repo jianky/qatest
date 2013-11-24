4cd938c38cd9d5c2d8d98a8fa472c229753df968
QA TEST <qatest@QAs-iMac.local>
2013-11-24 10:59:10 +0800

    Initial Commit

---
+++ test1Tests/test1Tests.m
@@ -0,0 +1,34 @@
+//
+//  test1Tests.m
+//  test1Tests
+//
+//  Created by QA TEST on 13-11-24.
+//  Copyright (c) 2013年 QA TEST. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test1Tests : XCTestCase
+
+@end
+
+@implementation test1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
